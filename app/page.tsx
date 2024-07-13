ab6260f50fc7ab29e84f24638a087e1f0121e271
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2024-07-13 14:14:12 +0530

    add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { useState } from 'react';
-import { Carousel } from "@material-tailwind/react";
 
 const Home = () => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
@@ -26,41 +25,8 @@ const Home = () => {
       </div>
 
       {/* Right Column */}
-      <div style={{ backgroundColor: '#F7F3E9' }} className="w-full md:w-1/2 h-screen bg-green-900 flex items-center justify-center pt-0 px-0 md:px-0">
-        <Carousel
-          className="rounded-xl w-[500px] h-[500px]"
-          onChange={setActiveIndex}
-          activeIndex={activeIndex}
-          navigation={({ setActiveIndex, activeIndex, length }) => (
-            <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-              {new Array(length).fill("").map((_, i) => (
-                <span
-                  key={i}
-                  className={`block h-1 cursor-pointer rounded-2xl transition-all ${
-                    activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
-                  }`}
-                  onClick={() => setActiveIndex(i)}
-                />
-              ))}
-            </div>
-          )}
-        >
-          <img
-            src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-            alt="image 1"
-            className="h-full w-full object-cover"
-          />
-          <img
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-            alt="image 2"
-            className="h-full w-full object-cover"
-          />
-          <img
-            src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-            alt="image 3"
-            className="h-full w-full object-cover"
-          />
-        </Carousel>
+      <div  style={{ backgroundColor: '#F7F3E9' }} className="w-full md:w-1/2 h-screen bg-green-900 flex items-center justify-center pt-0 px-0 md:px-0">
+      <img className='w-[]500px h-[500px]' src="https://i.pinimg.com/564x/1d/64/7e/1d647e898e538634379366ef24f572ce.jpg" alt="" />
       </div>
     </div>
   );
